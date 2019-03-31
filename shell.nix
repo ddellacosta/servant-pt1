a2d76a4c7a38b21eedb23c93d87cde5ddfe8075f
(cdep)illabout <cdep.illabout@gmail.com>
2019-03-31 11:44:02 +0900

    Make you could make this change, but it is not a huge improvement.

--- shell.nix
+++ shell.nix
@@ -3,7 +3,8 @@
 let
   inherit (nixpkgs) pkgs;
   hpkgs = pkgs.haskell.packages.${compiler};
+  servant-pt1 = pkgs.callPackage ./. {};
 in
-(hpkgs.callCabal2nix "servant-pt1" ./. {}).env.overrideAttrs(drv: {
+servant-pt1.env.overrideAttrs (drv: {
   buildInputs = drv.buildInputs ++ [ hpkgs.cabal-install pkgs.dhall pkgs.postgresql ];
 })
